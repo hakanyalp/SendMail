cd7bbcd922014096dacd83255cec08eb3c868cec
hakanyalp <hakanyavuzalp@gmail.com>
2019-03-08 23:59:40 +0300

    proje eklendi

---
+++ mail_gonder/mail_gonder/Form1.Designer.cs
@@ -0,0 +1,205 @@
+﻿namespace mail_gonder
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button6 = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblSonMailTarihi = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(200, 66);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Seçilenlere Mail Gönder";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 210);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(200, 36);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "İçeri Aktar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Location = new System.Drawing.Point(218, 12);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(311, 445);
+            this.checkedListBox1.TabIndex = 2;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 252);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(200, 36);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Seçilenleri Kaldır";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(218, 465);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(130, 37);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Tümünü seç";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(399, 465);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(130, 37);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "Tümünü kaldır";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 393);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(200, 26);
+            this.textBox1.TabIndex = 6;
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(12, 425);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(200, 32);
+            this.button6.TabIndex = 7;
+            this.button6.Text = "Ekle";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(166, 544);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(56, 26);
+            this.numericUpDown1.TabIndex = 8;
+            this.numericUpDown1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(12, 534);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(154, 47);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Gönderdikten sonra beklenilecek sn";
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(316, 531);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(201, 26);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Son mail gönderme zamanı";
+            // 
+            // lblSonMailTarihi
+            // 
+            this.lblSonMailTarihi.Location = new System.Drawing.Point(316, 555);
+            this.lblSonMailTarihi.Name = "lblSonMailTarihi";
+            this.lblSonMailTarihi.Size = new System.Drawing.Size(201, 26);
+            this.lblSonMailTarihi.TabIndex = 11;
+            this.lblSonMailTarihi.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(550, 586);
+            this.Controls.Add(this.lblSonMailTarihi);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.checkedListBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Mail Gönderme";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblSonMailTarihi;
+    }
+}
+
---
+++ mail_gonder/mail_gonder/Form1.cs
@@ -0,0 +1,316 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace mail_gonder
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MaximizeBox = false;
+            numericUpDown1.Value = wait_time / 1000;    // milisaniyeyi saniye cinsinden elde edebilmek için 1000e bölünür
+
+            try
+            {
+                StreamReader reader = new StreamReader("./mail_list.txt");
+                int repeat_count = 0;
+                List<string> err_mail = new List<string>();     //txt'deki hatalı mail adresleri burada tutulup log'lanacak
+                while (!reader.EndOfStream)
+                {
+                    string text = reader.ReadLine();
+
+                    // txt dosyamdaki maillerde ilk ve son 2 karakter silinmesi gerekiyor.
+                    text = text.Substring(1, text.Length - 2);
+
+                    bool repeat_control = false;
+                    bool err_control = false;
+                    for (int i = 0; i < checkedListBox1.Items.Count; i++)
+                    {
+                        repeat_control = false;
+
+                        // tekrar kontrolü yapılıyor
+                        if (text == checkedListBox1.Items[i].ToString())    // eğer eklenecek eleman listede varsa eklenmeyecek
+                        {
+                            repeat_control = true;
+                            repeat_count++;
+                            break;
+                        }
+                        //mail formatı uygun mu kontrolü yapılıyor
+                        else if (string.IsNullOrEmpty(text) || !Regex.IsMatch(text, pattern))   // boş değilse veya format uygun değilse kontrolü
+                        {
+                            err_control = true;
+                        }
+                    }
+                    if (!repeat_control && !err_control)
+                        checkedListBox1.Items.Add(text);
+
+                    if (err_control)    // gelen satır mail formatına uygun değilse log'lamak için listeye ekleniyor
+                        err_mail.Add(text);
+                }
+                reader.Close();
+            }
+            catch
+            {
+                // hiçbir şey yapma
+            }
+        }
+
+        int wait_time = 1000;   // milisecond
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //mail gönder
+
+            DialogResult dialogResult = MessageBox.Show("Seçili kişilere mail gönderilecek, emin misiniz?", "Mail Gönderilecek", MessageBoxButtons.YesNo);
+
+            bool mailhtml_control = true;       //mail.html dosyası var mı diye kontrol ediliyor
+            if (dialogResult == DialogResult.Yes)
+            {
+                for (int i = 0; i < checkedListBox1.CheckedItems.Count; i++)
+                {
+                    try
+                    {
+                        string senderEmail = "kodyazari0@gmail.com";  //mail adresi kullanıcı mail adresi girilecek
+                        string senderPassword = ".Q1w2e3r4";  //mail adresi kullanıcı şifresi girilecek
+
+                        SmtpClient client = new SmtpClient("smtp.gmail.com", 587);
+                        client.Timeout = 100000;
+                        client.DeliveryMethod = SmtpDeliveryMethod.Network;
+                        client.UseDefaultCredentials = false;
+                        client.Credentials = new NetworkCredential(senderEmail, senderPassword);
+                        client.EnableSsl = true;
+
+                        string toEmail;
+                        toEmail = checkedListBox1.CheckedItems[i].ToString();
+                        string subject = "Otomatik Mail";
+                        string emailBody = "";
+
+                        // mail gönderilecek döküman
+                        string fileName = "mail" + ".html";
+                        // Read using File.OpenText
+                        if (System.IO.File.Exists(fileName))
+                        {
+                            using (System.IO.StreamReader sr = System.IO.File.OpenText(fileName))
+                            {
+                                String input;
+                                while ((input = sr.ReadLine()) != null)
+                                {
+                                    emailBody += input + "\r\n";
+                                }
+                            }
+                        }
+                        if (emailBody == "")    // mail içeriği boşsa mail gönderme olmayacak
+                        {
+                            mailhtml_control = false;
+                            break;
+                        }
+
+                        MailMessage mailMessage = new MailMessage(senderEmail, toEmail, subject, emailBody);
+                        mailMessage.IsBodyHtml = true;
+
+                        mailMessage.BodyEncoding = UTF8Encoding.UTF8;
+
+                        client.Send(mailMessage);
+                        System.Threading.Thread.Sleep(wait_time);
+
+                        lblSonMailTarihi.Text = DateTime.Now.ToString();
+                    }
+                    catch(Exception ex)
+                    {
+                        MessageBox.Show(checkedListBox1.CheckedItems[i].ToString() + " kişisine mail gönderme başarısız oldu!");
+                    }
+                }
+            }
+            else if (dialogResult == DialogResult.No)
+            {
+                MessageBox.Show("Mail gönderme iptal edildi");
+            }
+
+            if (!mailhtml_control)
+            {
+                MessageBox.Show("mail.html dosyası bulunamadı!");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //içeri aktar
+
+            OpenFileDialog ofd = new OpenFileDialog { Filter = "Metin Dosyası | *.txt;*.html" };
+            ofd.ShowDialog();
+            if (!string.IsNullOrEmpty(ofd.FileName))
+            {
+                StreamReader reader = new StreamReader(ofd.FileName);
+                int repeat_count = 0;
+                List<string> err_mail = new List<string>();     //txt'deki hatalı mail adresleri burada tutulup log'lanacak
+                while (!reader.EndOfStream)
+                {
+                    string text = reader.ReadLine();
+
+                    // txt dosyamdaki maillerde ilk ve son 2 karakter silinmesi gerekiyor.
+                    text = text.Substring(1, text.Length - 2);
+
+                    bool repeat_control = false;
+                    bool err_control = false;
+                    for (int i = 0; i < checkedListBox1.Items.Count; i++)
+                    {
+                        repeat_control = false;
+
+                        // tekrar kontrolü yapılıyor
+                        if (text == checkedListBox1.Items[i].ToString())    // eğer eklenecek eleman listede varsa eklenmeyecek
+                        {
+                            repeat_control = true;
+                            repeat_count++;
+                            break;
+                        }
+                        //mail formatı uygun mu kontrolü yapılıyor
+                        else if (string.IsNullOrEmpty(text) || !Regex.IsMatch(text, pattern))   // boş değilse veya format uygun değilse kontrolü
+                        {
+                            err_control = true;
+                        }
+                    }
+                    if (!repeat_control && !err_control)
+                        checkedListBox1.Items.Add(text);
+
+                    if (err_control)    // gelen satır mail formatına uygun değilse log'lamak için listeye ekleniyor
+                        err_mail.Add(text);
+                }
+                reader.Close();
+
+                //hata mesajı verilecek kısım
+                if (repeat_count > 0 || err_mail.Count > 0)
+                {
+                    string err_message = "";
+                    if (repeat_count > 0 && err_mail.Count > 0)     //hem listede aynı kişi var, hem de mail formatı uygun olmayanlar var
+                    {
+                        err_message += repeat_count + " kişi listede kayıtlı olduğu için, " + err_mail.Count() + " mail adresi formatı uygun olmadığı için eklenemedi. Ayrıca hatalı mail adreslerinin log kaydı yapıldı.";
+
+                        //burada ayrıca eklenemeyenlerin logu tutulacak
+                        for (int i = 0; i < err_mail.Count; i++)
+                        {
+                            File.AppendAllText("log.txt", err_mail[i] + Environment.NewLine);
+                        }
+                    }
+                    else if (repeat_count > 0 && err_mail.Count == 0)      //listede aynı kişi var, bütün maillerin formatı doğru
+                    {
+                        err_message += repeat_count + " kişi listede kayıtlı olduğu için eklenemedi.";
+                    }
+                    else if (repeat_count == 0 && err_mail.Count > 0)        //tekrar eden yok, mail formatı yanlış olan var
+                    {
+                        err_message += err_mail.Count() + " mail adresi formatı uygun olmadığı için eklenemedi. Hatalı mail adreslerinin log kaydı yapıldı";
+
+                        //burada ayrıca eklenemeyenlerin logu tutulacak
+                        for (int i = 0; i < err_mail.Count; i++)
+                        {
+                            File.AppendAllText("log.txt", err_mail[i] + Environment.NewLine);
+                        }
+                    }
+                    MessageBox.Show(err_message);
+                }
+            }
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //seçilenleri sil
+            int count = checkedListBox1.CheckedItems.Count;
+            List<string> deleting = new List<string>();
+            for (int i = 0; i < checkedListBox1.CheckedItems.Count; i++)
+            {
+                deleting.Add(checkedListBox1.CheckedItems[i].ToString());
+            }
+            bool delete_control = false;
+            for (int i = 0; i < deleting.Count; i++)
+            {
+                checkedListBox1.Items.Remove(deleting[i]);
+                delete_control = true;
+            }
+            if (!delete_control)
+                MessageBox.Show("Silinecek eleman seçmelisiniz");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            //tümünü seç
+            for (int i = 0; i < checkedListBox1.Items.Count; i++)
+            {
+                checkedListBox1.SetItemChecked(i, true);
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            //tümünü kaldır
+            for (int i = 0; i < checkedListBox1.Items.Count; i++)
+            {
+                checkedListBox1.SetItemChecked(i, false);
+            }
+        }
+        //mail pattern
+
+        string pattern = @"^(([\w-]+\.)+[\w-]+|([a-zA-Z]{1}|[\w-]{2,}))@"
+   + @"((([0-1]?[0-9]{1,2}|25[0-5]|2[0-4][0-9])\."
+   + @"([0-1]?[0-9]{1,2}|25[0-5]|2[0-4][0-9])\."
+   + @"([0-1]?[0-9]{1,2}|25[0-5]|2[0-4][0-9])\."
+   + @"([0-1]?[0-9]{1,2}|25[0-5]|2[0-4][0-9])){1}|"
+   + @"([a-zA-Z]+[\w-]+\.)+[a-zA-Z]{2,4})$";
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            //kullanıcının girdiği mail adresini ekle
+
+            string data = textBox1.Text;
+
+            if (!string.IsNullOrEmpty(data) && Regex.IsMatch(data, pattern))
+            {
+                bool repeat_control = false;
+                for (int i = 0; i < checkedListBox1.Items.Count; i++)
+                {
+                    if (data == checkedListBox1.Items[i].ToString())    // eğer eklenecek eleman listede varsa eklenmeyecek
+                    {
+                        repeat_control = true;
+                        break;
+                    }
+                }
+                if (!repeat_control)
+                {
+                    checkedListBox1.Items.Add(data);
+                    textBox1.Clear();
+                }
+                else
+                    MessageBox.Show("Kişi zaten listede kayıtlı!");
+            }
+
+            else
+            {
+                MessageBox.Show("Mail adresi geçerli değildir!");
+            }
+
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == 13)
+            {
+                button6.PerformClick();
+            }
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            wait_time = Convert.ToInt32(numericUpDown1.Value) * 1000;
+        }
+    }
+}
